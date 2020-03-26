9031a242a64bad4847042b7af7a7f145c6dc9758
beluhin.an <beluhin.an@yandex.ru>
2020-03-26 20:34:54 +0300

    temp com

---
+++ game/snake.cpp
@@ -0,0 +1,18 @@
+#include "snake.h"
+
+Snake::Snake()
+{
+
+}
+
+Snake::~Snake()
+{
+}
+
+void Snake::init(int x = 7, int y = 7, char fill_char = '#')
+{
+	coord.push_back(make_pair(x, y));
+	for (int i = 0; i < START_LEN; i++) {
+		coord.push_back(make_pair(coord.end));
+	}
+}
--- game/snake.h
+++ game/snake.h
@@ -1,6 +1,23 @@
 #ifndef __SNAKE_H__
 #define __SNAKE_H__
 
+#define START_LEN 4
+
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+class Snake
+{
+public:
+	Snake();
+	~Snake();
+	void init(int, int, char);
+private:
+	vector<pair<int, int>> coord;
+};
+
 
 
 #endif
